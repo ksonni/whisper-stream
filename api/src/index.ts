f386ad702dca1d56b9807ca3bfd0e17dbb9e4825
Keshav Sonni <keshav.vm97@gmail.com>
2023-12-23 03:55:42 +0000

    API: revert to TS node setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,1 @@
-Bun.serve({
-    fetch(req, server) {
-        if (server.upgrade(req)) {
-            return;
-        }
-        return new Response('WS upgrade failed', { status: 500 });
-    }, // upgrade logic
-    websocket: {
-      message(ws, message) {
-        console.log('Got a message!', message);
-      }, // a message is received
-      open(ws) {}, // a socket is opened
-      close(ws, code, message) {}, // a socket is closed
-      drain(ws) {}, // the socket is ready to receive more data
-    },
-    port: 3000,
-});
+console.log('TS node running');
