184ebc50a02307d7f3adb37ead11e355542ee3bd
Keshav Sonni <keshav.vm97@gmail.com>
2023-12-24 23:25:14 +0000

    API: switch to transformers model

--- api/src/transcribe.py
+++ api/src/transcribe.py
@@ -1,12 +1,21 @@
 import threading
 import os, stat
-import whisper
 import uuid
 import time
+import torch
 
-from whisper.audio import load_audio
+from transformers import pipeline
 
-model = whisper.load_model("base.en")
+def make_pipeline():
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model="openai/whisper-base.en",
+        torch_dtype=torch.float16,
+        device="cuda:0" if torch.cuda.is_available() else "mps",
+    )
+    return pipe
+
+lib_transcribe = make_pipeline()
 
 async def transcribe_opus(data):
     start = time.time()
@@ -42,8 +51,7 @@ def write_to_pipe(path, data):
 
 def transcribe(path, results):
     try:
-        ar = load_audio(path)
-        results[0] = model.transcribe(ar, fp16=False)
+        results[0] = lib_transcribe(path) 
     except Exception as e:
         print(f'Transcribe of pipe {path} failed', e)
         results[0] = { "error": True }
